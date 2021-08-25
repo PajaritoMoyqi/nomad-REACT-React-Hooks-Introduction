a2ec296c1e169710917a6df23898effb98be3c2a
Pajarito Moyqi <PajaritoMoyqi@protonmail.ch>
2021-08-25 16:36:20 +0900

    first custom useEffect

--- client.jsx
+++ client.jsx
@@ -1,12 +1,13 @@
 const React = require('react')
 const ReactDOM = require('react-dom');
 
-import { App } from './src/components/App';
+// import { App } from './src/components/App';
 // import { UseInputApp } from './src/components/UseInputApp';
 // import { UseTabsApp } from './src/components/UseTabsApp';
+import { UseTitleApp } from './src/components/UseTitleApp';
 
 ReactDOM.render(
-  <App />,
+  <UseTitleApp />,
   document.getElementById('root')
 );
 
